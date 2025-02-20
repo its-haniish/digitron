5e63c29c4b0345ac9ee9e0fae455680f9509c1cd
its-haniish <itshaniish@gmail.com>
2025-02-20 17:01:05 +0530

    changed the routes to /student & /events

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,28 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const eventRoutes = require("./routes/event.routes"); 
-const studentRoutes = require("./routes/student.routes");
+    require("dotenv").config();
+    const express = require("express");
+    const mongoose = require("mongoose");
+    const eventRoutes = require("./routes/event.routes"); 
+    const studentRoutes = require("./routes/student.routes");
 
-const app = express();
-const PORT = process.env.PORT || 8083;
+    const app = express();
+    const PORT = process.env.PORT || 8083;
 
-// Middleware
-app.use(express.json());
+    // Middleware
+    app.use(express.json());
 
-// Routes
-app.use("/api/events", eventRoutes);
-app.use("/api/students", studentRoutes); 
+    // Routes
+    app.use("/events", eventRoutes);
+    app.use("/students", studentRoutes); 
 
-// Connect to MongoDB and Start the Server
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log("Connected to the database.");
-        app.listen(PORT, () => {
-            console.log(`The server is live at: http://localhost:${PORT}`);
+    // Connect to MongoDB and Start the Server
+    mongoose.connect(process.env.MONGO_URI)
+        .then(() => {
+            console.log("Connected to the database.");
+            app.listen(PORT, () => {
+                console.log(`The server is live at: http://localhost:${PORT}`);
+            });
+        })
+        .catch((err) => {
+            console.log("Error connecting to the database...");
+            console.log(err);
         });
-    })
-    .catch((err) => {
-        console.log("Error connecting to the database...");
-        console.log(err);
-    });
