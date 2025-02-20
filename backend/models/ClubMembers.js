040a062574d337e5f36f5ca9a9b56bb9d9937844
Keshav Kumar <94639526+DeveloperKeshavKumar@users.noreply.github.com>
2025-02-20 22:55:05 +0530

    add club members schema

---
+++ backend/models/ClubMembers.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+
+const clubMembersSchema = new mongoose.Schema({
+    clubMemberName: {
+        type: String,
+        required: true
+    },
+    clubMemberEmail: {
+        type: String,
+        required: true
+    },
+    clubMemberPhone: {
+        type: String,
+        required: true
+    },
+    clubMemberPosition: {
+        type: true,
+        required: true
+    },
+    clubMemberSemester: {
+        type: Number,
+        required: true
+    },
+    clubMemberBatch: {
+        type: Number,
+        required: true
+    },
+    about: {
+        type: String,
+        required: true
+    },
+    profilePic: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Number,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Number,
+        default: Date.now,
+    },
+    socialMediaLinks: {
+        linkedin: {
+            type: String,
+            required: true
+        },
+        instagram: {
+            type: String,
+            required: true
+        },
+        twitter: {
+            type: String,
+            required: true
+        },
+        github: {
+            type: String,
+            required: true
+        },
+        portfolio: {
+            type: String
+        }
+    }
+
+});
+
+module.exports = mongoose.model('ClubMembers', clubMembersSchema);
