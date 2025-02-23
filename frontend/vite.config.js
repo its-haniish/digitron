fca9f7c6a2fd78697c0fc70c187559d3528addef
its-haniish <itshaniish@gmail.com>
2025-02-23 23:45:33 +0530

    hello world

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  port: 8085, // Ensure this is your desired port
+  host: true,  // Allows external access
+  allowedHosts: ['digitron.rocks'], // Add your domain here
   plugins: [react(), tailwindcss()],
 });
+
